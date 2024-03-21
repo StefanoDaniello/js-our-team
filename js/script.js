f3ea79175fd5698531dc13008d47041329d08221
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-03-21 15:48:14 +0100

    fixed img

--- js/script.js
+++ js/script.js
@@ -31,34 +31,34 @@ Barbara Ramos	Graphic Designer	         barbara-ramos-graphic-designer.jpg
 
 let team=[
     {
-        foto: './img/wayne-barnett-founder-ceo.jpg',
+        foto: 'img/wayne-barnett-founder-ceo.jpg',
         nome : 'Wayne Barnett',
         ruolo : 'Founder & CEO'
     },
     {
-        foto: './img/wayne-barnett-founder-ceo.jpg',
-        nome : 'Wayne Barnett',
-        ruolo : 'Founder & CEO'
+        foto: 'img/angela-caroll-chief-editor.jpg',
+        nome : 'Angela Caroll',
+        ruolo : ' Chief Editor'
     },  
     {
-        foto: './img/wayne-barnett-founder-ceo.jpg',
-        nome : 'Wayne Barnett',
-        ruolo : 'Founder & CEO'
+        foto: 'img/ walter-gordon-office-manager.jpg',
+        nome : 'Walter Gordon',
+        ruolo : 'Office Manager'
     },
     {
-        foto: './img/wayne-barnett-founder-ceo.jpg',
-        nome : 'Wayne Barnett',
-        ruolo : 'Founder & CEO'
+        foto: 'img/angela-lopez-social-media-manager.jpg',
+        nome : 'Angela Lopez',
+        ruolo : ' Social Media Manager'
     },
     {
-        foto: './img/wayne-barnett-founder-ceo.jpg',
-        nome : 'Wayne Barnett',
-        ruolo : 'Founder & CEO'
+        foto: 'img/scott-estrada-developer.jpg',
+        nome : 'Scott Estrada',
+        ruolo : 'Developer'
     },
     {
-        foto: './img/wayne-barnett-founder-ceo.jpg',
-        nome : 'Wayne Barnett',
-        ruolo : 'Founder & CEO'
+        foto: 'img/barbara-ramos-graphic-designer.jpg',
+        nome : 'Barbara Ramos',
+        ruolo : 'Graphic Designer'
     }
 
 ];
@@ -69,7 +69,8 @@ for(let value of team){
  let boxes=document.createElement('div')
  boxes.classList.add('box')
  boxes.innerHTML=`
-${value.foto} <br> ${value.nome} <br> ${value.ruolo}
+ <img src="${value.foto}" class="foto">
+ <br> ${value.nome} <br> ${value.ruolo}
  `
  stcontainer.appendChild(boxes)
 }
